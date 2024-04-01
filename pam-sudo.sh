98474ae8bbd0b52af4f37890bdd311cb60152247
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-03-31 23:00:52 -0500

    Update pam-sudo.sh

--- pam-sudo.sh
+++ pam-sudo.sh
@@ -2,6 +2,7 @@
 
 #: Requires root.
 #: Requires Dependancies: sudo
+#: Has trouble running from a webserver user.
 #: Example command: bash ./pam-sudo.sh usernamestring passwordstring
 
 printf "%s" "$2" | sudo -n -S -u "$1" true
